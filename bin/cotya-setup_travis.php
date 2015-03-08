6d8e4f8ec1a95017ae3601ca16c810ce181fa258
Flyingmana <flyingmana@googlemail.com>
2015-03-08 20:46:32 +0100

    add prototype for mage2 setup

--- bin/cotya-setup_travis.php
+++ bin/cotya-setup_travis.php
@@ -21,7 +21,23 @@ if (strpos($magentoTargetVersion, '1.')===0) {
     $passthru('vendor/bin/mage-ci install test-root $MAGENTO magento_test -c -t -r http://mage-ci.ecomdev.org');
     $passthru('vendor/bin/mage-ci install-module test-root '.$moduleRoot);
 } elseif (strpos($magentoTargetVersion, '2.')===0) {
-    echo "I think we need a way to install mage2 in a similar way".PHP_EOL;
+    $passthru('wget https://github.com/magento/magento2/archive/0.42.0-beta10.tar.gz');
+    $passthru('tar -xzf 0.42.0-beta10.tar.gz');
+    rename('magento2-0.42.0-beta10', 'test-root');
+    rename(
+        'test-root/dev/tests/integration/etc/install-config-mysql.travis.php.dist',
+        'test-root/dev/tests/integration/etc/install-config-mysql.php'
+    );
+    chdir('test-root');
+    $passthru('composer install --no-interaction --dev');
+
+    chdir($moduleRoot);
+    $moduleName = simplexml_load_file('etc/module.xml')->xpath('module')[0]->getName();
+    $moduleDir = str_replace('_', DIRECTORY_SEPARATOR, $moduleName);
+    $passthru('cp -s ./ ./test-root/app/code/'.$moduleDir);
+    
+    //$passthru('php test-root/setup/index.php module-enable --modules='.$moduleName );
+    
 } else {
     fwrite(STDERR, "Version Identifier '$magentoTargetVersion' not recogniezed".PHP_EOL);
     die(1);
